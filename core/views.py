355fda3d3998e48d7eb819ccffc6a235fc1d57b2
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2025-01-16 17:21:41 +0330

    Added account settings

--- core/views.py
+++ core/views.py
@@ -10,6 +10,28 @@ from .models import Profile
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        image = request.FILES.get('image')
+        if not image:
+            image = user_profile.profileimg
+
+        bio = request.POST['bio']
+        location = request.POST['location']
+
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -36,11 +58,15 @@ def signup(request):
         user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
 
-        # Create a profile object for the new user
+        # Login the user
+        user_login = auth.authenticate(username=username, password=password)
+        auth.login(request, user_login)
+
+        # Create a profile object for the new user and redirect to the settings page
         user_model = User.objects.get(username=username)
         user_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
         user_profile.save()
-        return redirect('signup')
+        return redirect('settings')
 
     return render(request, 'signup.html')
 
